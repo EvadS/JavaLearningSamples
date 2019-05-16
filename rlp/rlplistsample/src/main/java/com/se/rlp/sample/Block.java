0f77622e1826ae4bb128cc96d407f2bf93f679c6
Evgeniy.Skiba <eskiba@s-pro.io>
2019-05-16 09:44:05 +0300

    changes on rlp test projects

--- rlp/rlplistsample/src/main/java/com/se/rlp/sample/Block.java
+++ rlp/rlplistsample/src/main/java/com/se/rlp/sample/Block.java
@@ -1,4 +1,104 @@
 package com.se.rlp.sample;
 
+import net.consensys.cava.bytes.Bytes;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
+
 public class Block {
+
+    private  int number;
+    private List<TransactionHash> transHashList;
+
+    public Block(int number, List<TransactionHash> transHash) {
+        this.number = number;
+        this.transHashList = transHash;
+    }
+
+    public static Block fromBytes(Bytes encoded) {
+        requireNonNull(encoded);
+        return RLP.decode(encoded, Block::readFrom);
+    }
+
+    public static Block readFrom(final RLPReader reader) {
+
+        System.out.println("read from ");
+        int curNum = reader.readInt();
+
+
+//        final List<TransactionHash> topics = in.readList(topicsReader ->
+//                Bytes.wrap(topicsReader.readValue()));
+//
+        List<TransactionHash> ommers = new ArrayList<>();
+
+    reader.readList((listReader, l) -> {
+
+         /////   Bytes bytes1 = listReader.readValue();
+        //////  Bytes bytes2 = listReader.readValue();
+         /////   Bytes bytes3 = listReader.readValue();
+
+            while (!listReader.isComplete()) {
+            int aa = 10;
+
+
+
+               Bytes bytes = listReader.readValue();
+                System.out.println("read: " +  bytes);
+               TransactionHash transHash = TransactionHash.fromBytes(bytes);
+             ////   ommers.add(transHash);
+           /// ommers.add(listReader.readValue(TransactionHash::readFrom));
+             //  ommers.add(listReader.readList(TransactionHash::/));
+                ommers.add(transHash);
+
+           }
+        });
+
+
+
+
+        return new Block(curNum,ommers);
+    }
+
+
+    public Bytes getBytes() {
+        return RLP.encode(this::writeTo);
+    }
+
+    private void writeTo(RLPWriter rlpWriter) {
+        rlpWriter.writeInt(number);
+
+        rlpWriter.writeList(topicsWriter -> {
+            System.out.println("topicsWriter");
+            for (TransactionHash  transHash : transHashList) {
+                System.out.printf(transHash.toString());
+                topicsWriter.writeValue(transHash.getBytes());
+            }
+        });
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Block block = (Block) o;
+        return number == block.number &&
+                Objects.equals(transHashList, block.transHashList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, transHashList);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public List<TransactionHash> getTransHashList() {
+        return transHashList;
+    }
 }
--- rlp/rlplistsample/src/main/java/com/se/rlp/sample/TransactionHash.java
+++ rlp/rlplistsample/src/main/java/com/se/rlp/sample/TransactionHash.java
@@ -1,4 +1,65 @@
 package com.se.rlp.sample;
 
+import net.consensys.cava.bytes.Bytes;
+import net.consensys.cava.rlp.RLP;
+import net.consensys.cava.rlp.RLPReader;
+import net.consensys.cava.rlp.RLPWriter;
+
+import java.util.List;
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
+
 public class TransactionHash {
+
+    private Bytes bytes;
+
+    public TransactionHash(Bytes bytes) {
+        requireNonNull(bytes);
+        this.bytes = bytes;
+    }
+
+    public static TransactionHash fromBytes(Bytes encoded) {
+        requireNonNull(encoded);
+        return RLP.decode(encoded, TransactionHash :: readFrom);
+    }
+
+    public static TransactionHash readFrom(RLPReader rlpReader) {
+        Bytes bytes = rlpReader.readValue();
+
+        return new TransactionHash(bytes);
+    }
+
+    public Bytes getBytes() {
+        return RLP.encode(this :: writeTo);
+    }
+
+    private void writeTo(RLPWriter rlpWriter) {
+        rlpWriter.writeValue(bytes);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TransactionHash that = (TransactionHash) o;
+        return Objects.equals(bytes, that.bytes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bytes);
+    }
+
+    @Override
+    public String toString() {
+        return "TransactionHash{" + "bytes=" + bytes + '}';
+    }
+
+    public void setBytes(Bytes bytes) {
+        this.bytes = bytes;
+    }
+
+    public static void readFrom2(RLPReader rlpReader, List<Object> objects) {
+    }
 }
