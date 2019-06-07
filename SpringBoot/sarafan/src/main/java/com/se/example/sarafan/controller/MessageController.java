935aa2cde872898d196de276baf3c26d679b1605
Evgeniy.Skiba <eugeniy.skiba@gmail.com>
2019-06-07 09:18:50 +0300

    Spring Boot Rest Application:  Добавляем базу данных в приложение с использованием JPA (по умолчанию провайдер - Hibernate). Также мы будем использовать для сериализации объектов библиотеку Spring по-умолчанию - Jackson.(Lombock,  @JsonView)

--- SpringBoot/sarafan/src/main/java/com/se/example/sarafan/controller/MessageController.java
+++ SpringBoot/sarafan/src/main/java/com/se/example/sarafan/controller/MessageController.java
@@ -1,72 +1,58 @@
 package com.se.example.sarafan.controller;
 
-import com.se.example.sarafan.exception.NotFoundException;
+import com.fasterxml.jackson.annotation.JsonView;
+import com.se.example.sarafan.domain.Message;
+import com.se.example.sarafan.domain.Views;
+import com.se.example.sarafan.repo.MessageRepo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("message")
 public class MessageController {
 
-    private  int counter = 4;
-    private  List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
-        add(new HashMap<String, String>() {{
-            put("id", "1");
-            put("text", "First message");
-        }});
-        add(new HashMap<String, String>() {{
-            put("id", "2");
-            put("text", "Second message");
-        }});
-        add(new HashMap<String, String>() {{
-            put("id", "3");
-            put("text", "Third message");
-        }});
-    }};
+    private final MessageRepo messageRepo;
+
+    @Autowired
+    public MessageController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
 
     @GetMapping
-    public List<Map<String, String>> list() {
-        return messages;
+    @JsonView(Views.IdName.class)
+    public List<Message> list() {
+        return messageRepo.findAll();
     }
 
     @GetMapping("{id}")
-    public  Map<String,String> getOne(@PathVariable String id){
-        return getMessage(id);
+    @JsonView(Views.FullMessage.class)
+    public Message getOne(@PathVariable("id") Message message) {
+        return message;
     }
 
-    private Map<String, String> getMessage(@PathVariable String id) {
-        return messages.stream()
-                .filter(message -> message.get("id").equals(id)).findFirst()
-                .orElseThrow(NotFoundException::new);
-    }
 
     @PostMapping
-    public  Map<String,String> create (@RequestBody Map<String,String> message){
-        message.put("id",String.valueOf(counter++));
+    public Message create(@RequestBody Message message) {
+        message.setCreationDate(LocalDateTime.now());
 
-        messages.add(message);
-        return  message;
+        return messageRepo.save(message);
     }
+
     @PutMapping("{id}")
-    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> message) {
-        Map<String, String> messageFromDb = getMessage(id);
+    public Message update(@PathVariable("id") Message messageFromDb,
+                          @RequestBody Message message) {
 
-        messageFromDb.putAll(message);
-        messageFromDb.put("id", id);
-
-        return messageFromDb;
+        BeanUtils.copyProperties(message, messageFromDb, "id");
+        return messageRepo.save(messageFromDb);
     }
 
 
     @DeleteMapping("{id}")
-    public void delete (@PathVariable String id){
-        Map<String,String> message = getMessage(id);
-
-        messages.remove(message);
+    public void delete(@PathVariable("id") Message message) {
+        messageRepo.delete(message);
     }
-
 }
---
+++ SpringBoot/sarafan/src/main/java/com/se/example/sarafan/domain/Views.java
@@ -0,0 +1,9 @@
+package com.se.example.sarafan.domain;
+
+public final class Views {
+    public interface Id {}
+
+    public interface IdName extends Id {}
+
+    public interface FullMessage extends IdNameit  {}
+}
