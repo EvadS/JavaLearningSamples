85bbc85b6e2486f2330131c8dc1328b57fb845f9
Evgeniy.Skiba <eskiba@s-pro.io>
2019-08-12 14:10:13 +0300

    added logging; changed banner

--- SpringBoot/flyway-demo/src/main/java/com/se/sample/flywaydemo/FlywayDemoApplication.java
+++ SpringBoot/flyway-demo/src/main/java/com/se/sample/flywaydemo/FlywayDemoApplication.java
@@ -1,13 +1,32 @@
 package com.se.sample.flywaydemo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlywayDemoApplication {
+public class FlywayDemoApplication  implements ApplicationRunner {
 
-	public static void main(String[] args) {
+    private static final Logger logger = LogManager.getLogger(FlywayDemoApplication.class);
+
+
+    public static void main(String[] args) {
 		SpringApplication.run(FlywayDemoApplication.class, args);
 	}
 
+
+    @Override
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+        logger.info("---------------------------------------");
+        logger.debug("Debugging log");
+        logger.info("Info log");
+        logger.warn("Hey, This is a warning!");
+        logger.error("Oops! We have an Error. OK");
+        logger.fatal("Damn! Fatal error. Please fix me.");
+        logger.info("---------------------------------------");
+
+    }
 }
