d41f60fc17c14355085268210070467db75ab62b
Evgeniy.Skiba <eskiba@s-pro.io>
2019-08-12 13:55:36 +0300

    sample for demostrate migration script to MYSQL

---
+++ SpringBoot/flyway-demo/src/main/resources/db/migration/V2__testdata.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO users(username, first_name, last_name) VALUES('callicoder', 'Rajeev', 'Singh');
+INSERT INTO users(username, first_name, last_name) VALUES('flywaytest', 'Flyway', 'Test');
