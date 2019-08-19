faf5465ef5a26321fa81a9a9e9130db4ebf4087d
Evgeniy Skiba <eskiba@s-pro.io>
2019-08-19 19:24:28 +0300

    initial second app

---
+++ SpringBoot/spring-boot-rest-clientcertificate/src/main/java/com/se/example/springbootrestclientcertificate/SpringBootRestClientcertificateApplication.java
@@ -0,0 +1,13 @@
+package com.se.example.springbootrestclientcertificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestClientcertificateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestClientcertificateApplication.class, args);
+    }
+
+}
