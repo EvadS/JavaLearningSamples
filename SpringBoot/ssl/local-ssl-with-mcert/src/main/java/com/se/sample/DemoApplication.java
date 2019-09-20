fd7dbaffd6a6b3bbe47a3623193b595c656be66a
Evgeniy Skiba <eskiba@s-pro.io>
2019-09-20 16:01:00 +0300

    refactored

--- SpringBoot/ssl/local-ssl-with-mcert/src/main/java/com/se/sample/DemoApplication.java
+++ SpringBoot/ssl/local-ssl-with-mcert/src/main/java/com/se/sample/DemoApplication.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@GetMapping("/")
-	public String helloWorld() {
-		return "Hello World";
-	}
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World";
+    }
 }
