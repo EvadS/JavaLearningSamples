2a0b71fdfc4f15683cf73cf551c8f53907ee6416
Evgeniy Skiba <eskiba@s-pro.io>
2019-09-26 18:03:54 +0300

    fixed profiles

---
+++ Docker/demo-sys-env/src/main/java/com/docker/example/HomeController.java
@@ -0,0 +1,53 @@
+package com.docker.example;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @Autowired
+    private Environment environment;
+
+
+    @Value("#{systemEnvironment['My_JAVA_OPTS']}")
+    private String host;
+
+
+    // work
+    @Value("${java.home}")
+    private String javaHome;
+
+
+    @Value("#{systemProperties['java.home']}")
+    private String javaHome2;
+
+
+    @Value("#{environment['JAVA_HOME']}")
+    private String javaHome3;
+
+    @Value("#{systemEnvironment['JAVA_HOME']}")
+    private String javaHome4;
+
+
+    @Value("#{ systemProperties['user.language'] }")
+    private String varOne;
+
+    @Value("#{ systemProperties['path'] }")
+    private String user;
+
+
+    @RequestMapping("/")
+    public String get() {
+        System.out.println("varOne  " + varOne);
+        System.out.println("path  " + user);
+        System.out.println("javaHome2  " + javaHome2);
+        System.out.println("javaHome3  " + javaHome3);
+        System.out.println("javaHome4  " + javaHome4);
+        return "javaHome2: "+javaHome2+ "; javaHome : " +javaHome+ "; Root WebApplicationContext: initialization completed: " + host ;
+    }
+}
