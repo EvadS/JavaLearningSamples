3788eae850dc531bdb137a8525f8f91409324a79
Evgeniy Skiba <eskiba@s-pro.io>
2019-09-26 11:28:42 +0300

    first version. STEP 1 DOCKER FILE

---
+++ Docker/docker-global-env/src/main/java/com/docker/sample/dockerglobalenv/DockerGlobalEnvApplication.java
@@ -0,0 +1,11 @@
+package com.docker.sample.dockerglobalenv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerGlobalEnvApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DockerGlobalEnvApplication.class, args);
+    }
+}
---
+++ Docker/docker-global-env/src/test/java/com/docker/sample/dockerglobalenv/DockerGlobalEnvApplicationTests.java
@@ -0,0 +1,16 @@
+package com.docker.sample.dockerglobalenv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerGlobalEnvApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
