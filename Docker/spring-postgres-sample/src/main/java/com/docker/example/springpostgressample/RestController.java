b116ccd9899ebbb49f5f1a58fb45bbb8eab38c6c
Evgeniy Skiba <eskiba@s-pro.io>
2019-09-28 16:25:44 +0300

    added index page

--- Docker/spring-postgres-sample/src/main/java/com/docker/example/springpostgressample/RestController.java
+++ Docker/spring-postgres-sample/src/main/java/com/docker/example/springpostgressample/RestController.java
@@ -1,10 +1,13 @@
 package com.docker.example.springpostgressample;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,7 +16,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(path = "/book")
 @ResponseBody
 public class RestController {
+
+    Logger logger = LoggerFactory.getLogger(RestController.class);
+
     private final RestService restService;
+
+    @GetMapping("/loggers")
+    public void testLogger(){
+
+        logger.info("info");
+        logger.debug("debug");
+        logger.error("error");
+        logger.warn("warn");
+
+    }
+
 
     @Autowired
     public RestController(RestService restService) {
