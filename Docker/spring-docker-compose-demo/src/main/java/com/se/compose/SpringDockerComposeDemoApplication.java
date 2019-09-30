e1ae3d13287229a1f2ab0e8846e0f4f906dd3f5f
Evgeniy Skiba <eskiba@s-pro.io>
2019-09-30 12:41:25 +0300

    first version with custom directories on container

---
+++ Docker/spring-docker-compose-demo/src/main/java/com/se/compose/SpringDockerComposeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.se.compose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerComposeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDockerComposeDemoApplication.class, args);
+    }
+
+}
