3266feef870c848d2daa73d8b76070e7d041ad12
Evgeniy Skiba <eskiba@s-pro.io>
2019-11-21 08:40:51 +0200

    removed untitled (temp) folder. Start work with react

---
+++ SpringBoot/FullStackSeries/Pools/polling-app-server/src/main/java/com/se/example/polls/controller/PollController.java
@@ -0,0 +1,64 @@
+package com.se.example.polls.controller;
+
+
+import com.se.example.polls.config.CurrentUser;
+import com.se.example.polls.config.UserPrincipal;
+import com.se.example.polls.model.PagedResponse;
+import com.se.example.polls.model.PollResponse;
+import com.se.example.polls.model.VoteRequest;
+import com.se.example.polls.repository.PollRepository;
+import com.se.example.polls.repository.UserRepository;
+import com.se.example.polls.repository.VoteRepository;
+import com.se.example.polls.service.PollService;
+import com.se.example.polls.util.AppConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import javax.validation.Valid;
+import java.net.URI;
+
+
+@RestController
+@RequestMapping("/api/polls")
+public class PollController {
+
+    @Autowired
+    private PollRepository pollRepository;
+
+    @Autowired
+    private VoteRepository voteRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PollService pollService;
+
+    private static final Logger logger = LoggerFactory.getLogger(PollController.class);
+
+    @GetMapping
+    public PagedResponse<PollResponse> getPolls(@CurrentUser UserPrincipal currentUser,
+                                                @RequestParam(value = "page", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int page,
+                                                @RequestParam(value = "size", defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int size) {
+        return pollService.getAllPolls(currentUser, page, size);
+    }
+
+    @GetMapping("/{pollId}")
+    public PollResponse getPollById(@CurrentUser UserPrincipal currentUser,
+                                    @PathVariable Long pollId) {
+        return pollService.getPollById(pollId, currentUser);
+    }
+
+    @PostMapping("/{pollId}/votes")
+    @PreAuthorize("hasRole('USER')")
+    public PollResponse castVote(@CurrentUser UserPrincipal currentUser,
+                                 @PathVariable Long pollId,
+                                 @Valid @RequestBody VoteRequest voteRequest) {
+        return pollService.castVoteAndGetUpdatedPoll(pollId, voteRequest, currentUser);
+    }
+
+}
---
+++ SpringBoot/FullStackSeries/Pools/polling-app-server/src/main/java/com/se/example/polls/model/RoleName.java
@@ -0,0 +1,7 @@
+package com.se.example.polls.model;
+
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
