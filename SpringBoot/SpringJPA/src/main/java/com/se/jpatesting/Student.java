3a94716dca860dd2f65f31446fab788332237cba
Evgeniy Skiba <eskiba@s-pro.io>
2019-12-03 09:22:14 +0200

    started work with spring boot jpa testing

---
+++ SpringBoot/SpringJPA/src/main/java/com/se/jpatesting/Student.java
@@ -0,0 +1,38 @@
+package com.se.jpatesting;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String passportNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    public void setPassportNumber(String passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+}
