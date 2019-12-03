2ddb5dcce29ccae3780ca63758d63776c1224694
Evgeniy Skiba <eskiba@s-pro.io>
2019-12-03 18:25:04 +0200

    junit sample

--- SpringBoot/unit-test-sample/src/main/java/com/se/example/unittestsample/components/SomeDependency.java
+++
@@ -1,7 +0,0 @@
-package com.se.example.unittestsample.components;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class SomeDependency {
-}
--- SpringBoot/unit-test-sample/src/main/java/com/se/example/unittestsample/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.se.example.unittestsample.repository;
-
-import com.se.example.unittestsample.dao.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
