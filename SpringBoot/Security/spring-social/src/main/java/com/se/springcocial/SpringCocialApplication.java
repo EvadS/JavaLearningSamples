6b8a9c07680bfe228e5f8d96a003d93c4442001d
Evgeniy Skiba <eskiba@s-pro.io>
2019-12-05 17:04:47 +0200

    started work with oauth

---
+++ SpringBoot/Security/spring-social/src/main/java/com/se/springcocial/SpringCocialApplication.java
@@ -0,0 +1,13 @@
+package com.se.springcocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCocialApplication.class, args);
+    }
+
+}
---
+++ SpringBoot/Security/spring-social/src/test/java/com/se/springcocial/SpringCocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.springcocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
