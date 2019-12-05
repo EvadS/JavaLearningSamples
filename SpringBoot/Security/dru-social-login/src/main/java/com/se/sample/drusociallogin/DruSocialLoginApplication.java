cea699610dc75d2f4f2a2f7d23b6bd112fe54331
Evgeniy Skiba <eskiba@s-pro.io>
2019-12-05 19:05:06 +0200

    started work

---
+++ SpringBoot/Security/dru-social-login/src/main/java/com/se/sample/drusociallogin/DruSocialLoginApplication.java
@@ -0,0 +1,13 @@
+package com.se.sample.drusociallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruSocialLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruSocialLoginApplication.class, args);
+    }
+
+}
---
+++ SpringBoot/Security/dru-social-login/src/test/java/com/se/sample/drusociallogin/DruSocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.sample.drusociallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DruSocialLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
