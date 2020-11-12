2ce7ebcca1913269f9d33d172f9e04ae5e2f9973
evgeniy_skiba <eugene.s@it-dimension.com>
2020-11-12 14:56:25 +0200

    Multipart exception handling h2 crud skeleton

--- spring-boot/base-mvc-service/src/main/java/com/se/sample/basemvcservice/annotation/RestGlobalExceptionHandler.java
+++ spring-boot/base-mvc-service/src/main/java/com/se/sample/basemvcservice/annotation/RestGlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.se.sample.basemvcservice.annotation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,20 +10,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.naming.AuthenticationException;
 import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class RestGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(RestGlobalExceptionHandler.class);
 
     @ExceptionHandler(MultipartException.class)
     @ResponseBody
     ResponseEntity<?> handleControllerException(HttpServletRequest request, Throwable ex) {
 
         HttpStatus status = getStatus(request);
+        logger.error(ex.getMessage());
 
         return new ResponseEntity(new CustomError("0x000123", "Attachment size exceeds the allowable limit! (10MB)"), status);
-
-
     }
 
     private HttpStatus getStatus(HttpServletRequest request) {
@@ -36,12 +40,9 @@ public class RestGlobalExceptionHandler extends ResponseEntityExceptionHandler {
         public String errCode;
         public String errDesc;
 
-
         public CustomError(String errCode, String errDesc) {
             this.errCode = errCode;
             this.errDesc = errDesc;
         }
-
     }
-
 }
