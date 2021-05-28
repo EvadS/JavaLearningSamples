eea2e615a31378fb4f62feffffd51a44f1a3f1bc
evgeniy_skiba <eugene.s@it-dimension.com>
2021-05-28 12:40:59 +0300

    Some changes

---
+++ notification/src/main/java/org/se/sample/App.java
@@ -0,0 +1,37 @@
+package org.se.sample;
+
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        Email from = new Email("no-reply@keabank.com");
+        String subject = "Sending with Twilio SendGrid is Fun";
+        Email to = new Email("evad.se.dev@gmail.com");
+        Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid("");
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            throw ex;
+        }
+    }
+}
